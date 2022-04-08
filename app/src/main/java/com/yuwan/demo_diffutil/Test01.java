d4db739f31110721c212a0fa228dae2021ea8a04
李江辉 <jianghui.li@yuwantech.com>
2022-04-08 10:02:48 +0800

    在dev分支上创建feature_ljh01分支,在这个分支上,提交内容

--- app/src/main/java/com/yuwan/demo_diffutil/Test01.java
+++ app/src/main/java/com/yuwan/demo_diffutil/Test01.java
@@ -4,6 +4,6 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("创建dev分支,并且在dev分支上,上传远程仓库");
         System.out.println("在dev分支上创建feature_ljh分支,在这个分支上,提交内容");
-
+        System.out.println("在dev分支上创建feature_ljh01分支,在这个分支上,提交内容");
     }
 }
